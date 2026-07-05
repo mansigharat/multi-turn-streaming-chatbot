ad7dbc49f131a9166563bd7f05cea793f4af5769
Mansi Gharat <mansigharat1909@gmail.com>
2026-07-05 23:42:34 +0530

    Chat bot complete

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+message = [{
+    "role" : "developer",
+    "content" : "You are a helpful assistant"
+    
+}]
+
+while True:
+    user_input = input("You : ")
+
+    if user_input.lower() == "exit":
+        break
+
+    message.append(
+        {"role" : "user", "content" : user_input}
+    )
+
+    response = client.responses.create(
+    model="openai/gpt-oss-120b",
+    input= message,
+    stream=True
+)
+
+    full_reply = ""
+    print("Bot: ", end="")
+    for event in response:
+        if event.type == "response.output_text.delta":
+            print(event.delta, end="", flush=True)
+            full_reply += event.delta
+
+    print()
+    message.append(
+        {"role" : "assistant", "content" : full_reply}
+    )
